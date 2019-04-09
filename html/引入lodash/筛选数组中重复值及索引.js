3de325c9bf4039430c8b603a64b0f24b760eda94
Fengye.Ren <AS40879@yeah.net>
2019-04-09 10:30:44 +0800

    筛选数组中重复值及索引

---
+++ html/引入lodash/筛选数组中重复值及索引.js
@@ -0,0 +1,10 @@
+
+
+const data = ["a", "a", "b", "c", "c", "a"]
+
+const result = _.pickBy(
+    data.reduce(function (result,current,index) {
+        a.indexOf(current, index) >= 0 ? _.set(result, current, [...result[current] || [], index]) && result : result, {}
+    }), x => x.length > 1)
+
+console.log(result)
