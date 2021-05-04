8eea7d2aa508c77e1f182185341864be88d18e4c
winnd <AS40879@yeah.net>
2021-05-04 13:01:12 +0800

    (更新)更新原型式继承

--- --js研究/--es5,es6/原型与constructor/原型与constructor.js
+++ --js研究/--es5,es6/原型与constructor/原型与constructor.js
@@ -10,7 +10,7 @@
  *        __proto__: Object
  * ```
  */
-function AA () {}
+function AA() {}
 
 const a = new AA()
 
@@ -18,3 +18,35 @@ const result1 = a.__proto__ === AA.prototype
 const result2 = a.__proto__.constructor === AA
 
 console.log(result1, result2)  // true, true
+
+
+// ① 原型继承
+function A(name) {
+    this.name = 'ccc'
+}
+
+A.prototype.myName = function () {
+    return this.name
+}
+
+function B(name, label) {
+    A.call(this, name)
+    this.label = label
+}
+
+/**
+ * 之前如果B.prototype里有东西会被丢弃掉，所以B.prototype.myLabel只能定义在这句话后面
+ * - 最好是用Object.setPrototypeOf(B.prototype,A.prototype)来进行扩展
+ * @type {A|*}
+ */
+B.prototype = Object.create(A.prototype)
+
+B.prototype.myLabel = function () {
+    return this.label
+}
+var b = new B('b', 'obj b')
+console.log({
+    name: b.myName(),
+    label: b.myLabel()
+})
+
