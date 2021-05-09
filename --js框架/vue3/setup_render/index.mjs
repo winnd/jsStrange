2b948cd32fb98a8a025692d1770abbbe3710a8e6
winnd <AS40879@yeah.net>
2021-05-09 15:20:42 +0800

    (更新) 增加说明

--- --js框架/vue3/setup_render/index.mjs
+++ --js框架/vue3/setup_render/index.mjs
@@ -1,17 +1,15 @@
-import {effectWatch} from '../reactive响应式实现/reactive.mjs'
 import {mountElement} from '../render/index.mjs'
 
 export function createApp(rootComponent) {
     return {
         mount(rootContainer) {
             const context = rootComponent.setup()
-            effectWatch(() => {
-                rootContainer.innerHTML = ''
-                const subTree = rootComponent.render(context)
-                console.log(subTree)
-                mountElement(subTree, rootContainer)
-                // rootContainer.append(element)
-            })
+            // effectWatch(() => {
+            rootContainer.innerHTML = ''
+            const subTree = rootComponent.render(context)
+            mountElement(subTree, rootContainer)
+            // rootContainer.append(element)
+            // })
         },
     }
 }
